79ac314f284a9ab2c8a39da7143697497131553b
alyssayc14@gmail.com <alyssayc@mit.edu>
2017-11-24 03:12:23 -0500

    added color gradient feedback

--- display.py
+++ display.py
@@ -134,7 +134,9 @@ class BeatMatchDisplay(InstructionGroup):
         self.gem_data = gem_data
         self.barline_data = barline_data
         self.beat_data = beat_data
-        self.action_colors = {} # Dictionary that stores the current color of the gems that passed, keyed by lane.
+        
+        # Dictionary that stores the current color of the gems.
+        self.action_colors = self.initialize_action_colors()
 
         self.add_header()
         self.add_nowbar()
@@ -146,6 +148,16 @@ class BeatMatchDisplay(InstructionGroup):
         self.ps = ps
 
         self.paused = True
+
+    def initialize_action_colors(self):
+        # Pass Color: Gray Color(.8,.8,.8)
+        action_colors = {}
+
+        for lane in self.gem_data:
+            num_gems = len(self.gem_data[lane])
+            action_colors[lane] = [[Color(0.,0.,0.), 0] for i in range(num_gems)]
+
+        return action_colors
 
     def draw_objects(self):
         self.trans = Translate()
@@ -210,39 +222,32 @@ class BeatMatchDisplay(InstructionGroup):
         y = lane_to_y_pos(lane, self.num_lanes)
         length = (NOW_PIXEL+10) - (x + self.trans.x)
         
-        color = False
-        if lane not in self.action_colors:
-            self.action_colors[lane] = {}
-
-        if gem_idx in self.action_colors[lane]:
-            color = self.action_colors[lane][gem_idx] 
-            
-            if action == 'pass':
-                color.r = (color.r+0.8)/2
-                color.g = (color.g+0.8)/2
-                color.b = (color.b+0.8)/2
-            elif action == 'on':
-                color.r = (color.r+0.)/2
-                color.g = (color.g+0.8)/2
-                color.b = (color.b+0.)/2
-            elif action == 'off':
-                color.r = (color.r+0.8)/2
-                color.g = (color.g+0)/2
-                color.b = (color.b+0.)/2
-
-        else:
-            if action == 'pass':
-                color = Color(.8,.8,.8)
-            elif action == 'on':
-                color = Color(0.,.8,0.)
-            elif action == 'off':
-                color = Color(0.8,0.,0.)
+        print('before: {}'.format(self.action_colors[lane][gem_idx][1]))
+        self.action_colors[lane][gem_idx][1] += 1
+        color, denom = self.action_colors[lane][gem_idx]
+        print('after: {}'.format(self.action_colors[lane][gem_idx][1]))             
         
-            self.action_colors[lane][gem_idx] = color
+        if action == 'pass':
+            color.r = (color.r+0.1)/denom
+            color.g = (color.g+0.)/denom
+            color.b = (color.b+0.)/denom
+        elif action == 'on':
+            color.r = (color.r+0.)/denom
+            color.g = (color.g+0.1)/denom
+            color.b = (color.b+0.)/denom
+        elif action == 'off':
+            color.r = (color.r+0.1)/denom
+            color.g = (color.g+0)/denom
+            color.b = (color.b+0.)/denom
 
-        if color:
-            gem = GemDisplay(pos=(x, y), color=color, length=length, lyric=False)
-            self.add(gem)
+        # Normalize color to be within 0-1
+        max_val = max(color.r, color.g, color.b)
+        color.r = color.r/max_val
+        color.g = color.g/max_val
+        color.b = color.b/max_val
+
+        gem = GemDisplay(pos=(x, y), color=color, length=length, lyric=False)
+        self.add(gem)
 
     # called by Player. Causes the right thing to happen
     def gem_hit(self, gem_idx, lane):
