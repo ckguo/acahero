473985fc80114e4bdae4a582d457ba58ae5cefd5
alyssayc14@gmail.com <alyssayc@mit.edu>
2017-11-29 01:54:13 -0500

    coordinated when widgets start

--- main.py
+++ main.py
@@ -4,14 +4,35 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from project import MainWidget
 from practice import MainWidgetPractice
 
+
 class MenuScreen(Screen):
     pass
 
 class PracticeScreen(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(PracticeScreen, self).__init__(**kwargs)
+        self.widget = MainWidgetPractice()
+        self.start = True 
+
+    def startWidget(self):
+        if self.start: 
+            self.add_widget(self.widget)
+            self.start = False
+
+        self.widget.toggle()
 
 class PerformScreen(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(PerformScreen, self).__init__(**kwargs)
+        self.widget = MainWidget()
+        self.start = True 
+
+    def startWidget(self):
+        if self.start: 
+            self.add_widget(self.widget)
+            self.start = False
+            
+        self.widget.toggle()
 
 class SettingsScreen(Screen):
     pass
